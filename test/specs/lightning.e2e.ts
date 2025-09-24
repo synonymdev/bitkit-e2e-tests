0cd471542594576458aab65b6135b2a6be7ab87d
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-09-24 13:49:02 +0200

    Add lightning_1 test

--- test/specs/lightning.e2e.ts
+++ test/specs/lightning.e2e.ts
@@ -1,6 +1,26 @@
 import BitcoinJsonRpc from 'bitcoin-json-rpc';
 import initElectrum from '../helpers/electrum';
-import { completeOnboarding, receiveOnchainFunds, expectTextVisible } from '../helpers/actions';
+import {
+  completeOnboarding,
+  receiveOnchainFunds,
+  expectTextVisible,
+  enterAddress,
+  multiTap,
+  tap,
+  dragOnElement,
+  elementById,
+  elementByIdWithin,
+  getReceiveAddress,
+  sleep,
+  expectTextWithin,
+  typeText,
+  confirmInputOnKeyboard,
+  swipeFullScreen,
+  getAddressFromQRCode,
+  getSeed,
+  restoreWallet,
+  mineBlocks,
+} from '../helpers/actions';
 import { reinstallApp } from '../helpers/setup';
 import { bitcoinURL, lndConfig } from '../helpers/constants';
 import {
@@ -77,5 +97,182 @@ describe('@lightning - Lightning', () => {
 
     // check channel status
     await checkChannelStatus();
+
+    // send funds to LDK, 0 invoice
+    const receive = await getReceiveAddress('lightning');
+    await swipeFullScreen('down');
+    const response = await lnd.sendPaymentSync({ paymentRequest: receive, amt: '10000' });
+    console.info({ response });
+    await elementById('ReceivedTransaction').waitForDisplayed();
+    await tap('ReceivedTransactionButton');
+    await sleep(500);
+    const totalBalance = await elementByIdWithin('TotalBalance-primary', 'MoneyText');
+    await expect(totalBalance).toHaveText('11 000'); // 1k onchain + 10k lightning
+    await expectTextWithin('ActivitySpending', '10 000');
+
+    // send funds to LDK, 111 sats invoice
+    await tap('Receive');
+    await tap('SpecifyInvoiceButton');
+    await tap('ReceiveNumberPadTextField');
+    await sleep(100);
+    await multiTap('N1', 3);
+    await tap('ReceiveNumberPadSubmit');
+    const note1 = 'note 111';
+    await typeText('ReceiveNote', note1);
+    await confirmInputOnKeyboard();
+    await sleep(300); // wait for keyboard to hide
+    await tap('TagsAdd');
+    await typeText('TagInputReceive', 'rtag');
+    await tap('ReceiveTagsSubmit');
+    await sleep(300);
+    await tap('ShowQrReceive');
+    const invoice2 = await getAddressFromQRCode('lightning');
+    await swipeFullScreen('down');
+    await lnd.sendPaymentSync({ paymentRequest: invoice2 });
+    await elementById('ReceivedTransaction').waitForDisplayed();
+    await tap('ReceivedTransactionButton');
+    await sleep(500);
+    await expect(totalBalance).toHaveText('11 111'); // 1k onchain + 10 111 lightning
+    await expectTextWithin('ActivitySpending', '10 111');
+
+    // send funds to LND, 0 invoice
+    const note2 = 'zero';
+    const { paymentRequest: invoice1 } = await lnd.addInvoice({
+      memo: note2,
+    });
+    await enterAddress(invoice1);
+    await multiTap('N1', 3);
+    await tap('ContinueAmount');
+    await dragOnElement('GRAB', 'right', 0.95); // Swipe to confirm
+    await elementById('SendSuccess').waitForDisplayed();
+    await tap('Close');
+    await expect(totalBalance).toHaveText('11 000'); // 1k onchain + 10k lightning
+    await expectTextWithin('ActivitySpending', '10 000');
+
+    // send funds to LND, 10000 invoice
+    const value = '1000';
+    const { paymentRequest: invoice4 } = await lnd.addInvoice({
+      value: value,
+    });
+    await enterAddress(invoice4);
+    // Review & Send
+    const reviewAmt = await elementByIdWithin('ReviewAmount-primary', 'MoneyText');
+    await reviewAmt.waitForDisplayed();
+    await expect(reviewAmt).toHaveText('1 000');
+    await tap('TagsAddSend');
+    await typeText('TagInputSend', 'stag');
+    await tap('SendTagsSubmit');
+    await sleep(500); // wait for keyboard to close
+    await dragOnElement('GRAB', 'right', 0.95); // Swipe to confirm
+    await elementById('SendSuccess').waitForDisplayed();
+    await tap('Close');
+    await expect(totalBalance).toHaveText('10 000'); // 1k onchain + 9k lightning
+    await expectTextWithin('ActivitySpending', '9 000');
+
+    // check tx history
+    await swipeFullScreen('up');
+    await expectTextWithin('ActivityShort-0', '1 000');
+    await expectTextWithin('ActivityShort-1', '111');
+    await expectTextWithin('ActivityShort-2', '111');
+    await tap('ActivityShort-1');
+    await expectTextWithin('InvoiceNote', note2);
+    await tap('NavigationClose');
+    await tap('ActivityShort-2');
+    await expectTextWithin('InvoiceNote', note1);
+    await tap('NavigationClose');
+
+    // check activity filters & tags
+    await tap('ActivityShowAll');
+    // All transactions
+    await expectTextWithin('Activity-1', '-');
+    await expectTextWithin('Activity-2', '-');
+    await expectTextWithin('Activity-3', '+');
+    await expectTextWithin('Activity-4', '+');
+    await expectTextWithin('Activity-5', '+');
+
+    // Sent, 2 transactions
+    await tap('Tab-sent');
+    await expectTextWithin('Activity-1', '-');
+    await expectTextWithin('Activity-2', '-');
+    await elementById('Activity-3').waitForDisplayed({ reverse: true });
+
+    // Received, 2 transactions
+    await tap('Tab-received');
+    await expectTextWithin('Activity-1', '+');
+    await expectTextWithin('Activity-2', '+');
+    await expectTextWithin('Activity-3', '+');
+    await elementById('Activity-4').waitForDisplayed({ reverse: true });
+
+    // Other, 0 transactions
+    await tap('Tab-other');
+    await elementById('Activity-1').waitForDisplayed({ reverse: true });
+
+    //--- skip due to: https://github.com/synonymdev/bitkit-android/issues/386 ---//
+    // // filter by receive tag
+    // await tap('Tab-all');
+    // await tap('TagsPrompt');
+    // await tap('Tag-rtag');
+    // await expectTextWithin('Activity-1', '+');
+    // await elementById('Activity-2').waitForDisplayed({reverse:true});
+    // await tap('Tag-rtag-delete');
+
+    // // filter by send tag
+    // await tap('TagsPrompt');
+    // await tap('Tag-stag');
+    // await expectTextWithin('Activity-1', '+');
+    // await elementById('Activity-2').waitForDisplayed({reverse:true});
+    // await tap('Tag-stag-delete');
+    //--- skip due to: https://github.com/synonymdev/bitkit-android/issues/386 ---//
+
+    await tap('NavigationBack');
+
+    // wipe and restore wallet
+    const seed = await getSeed();
+    // await waitForBackup();
+    await sleep(10_000); //temp wait (until we have a proper event for backup completion)
+    await restoreWallet(seed);
+
+    // check balance
+    await expect(totalBalance).toHaveText('10 000'); // 1k onchain + 9k lightning
+    await expectTextWithin('ActivitySpending', '9 000');
+
+    // check tx history
+    await swipeFullScreen('up');
+    await swipeFullScreen('up');
+    await expectTextWithin('ActivityShort-0', '1 000');
+    await expectTextWithin('ActivityShort-1', '111');
+    await expectTextWithin('ActivityShort-2', '111');
+    await tap('ActivityShort-1');
+    await expectTextWithin('InvoiceNote', note2);
+    await tap('NavigationClose');
+    await tap('ActivityShort-2');
+    await expectTextWithin('InvoiceNote', note1);
+    await tap('NavigationClose');
+
+    // check channel status
+    await tap('HeaderMenu');
+    await tap('DrawerSettings');
+    await tap('AdvancedSettings');
+    await tap('Channels');
+    await tap('Channel');
+    await expectTextWithin('TotalSize', '₿ 100 000');
+    await swipeFullScreen('up');
+    await elementById('IsUsableYes').waitForDisplayed();
+
+    // close channel
+    await tap('CloseConnection');
+    await tap('CloseConnectionButton');
+
+    await mineBlocks(rpc, 6);
+    await electrum?.waitForSync();
+    // https://github.com/synonymdev/bitkit-android/issues/268
+    // send - onchain - receiver sees no confetti — missing-in-ldk-node missing onchain payment event
+    // await elementById('ReceivedTransaction').waitForDisplayed();
+    await elementById('Channel').waitForDisplayed({ reverse: true });
+    await tap('NavigationBack');
+    await tap('NavigationClose');
+
+    await swipeFullScreen('up');
+    await expectTextWithin('ActivityShort-0', '9 000');
   });
 });
