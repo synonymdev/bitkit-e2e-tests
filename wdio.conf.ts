e47487e2bcdc94e1b490394a3841479b64866ec6
Piotr Stachyra <piotr.stachyra@gmail.com>
2026-01-15 22:40:51 +0100

    timeout for CI vs local

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -123,7 +123,7 @@ export const config: WebdriverIO.Config = {
   // baseUrl: 'http://localhost:8080',
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 30000,
+  waitforTimeout: process.env.CI ? 30000 : 10000,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
