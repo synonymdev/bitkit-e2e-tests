c38d1f0f17b77237abecc74ca5d3cfa10a8ab4e9
Piotr Stachyra <piotr.stachyra@gmail.com>
2026-02-24 17:23:13 +0100

    move to separate suite

--- test/specs/onchain.e2e.ts
+++ test/specs/onchain.e2e.ts
@@ -1,7 +1,6 @@
 import initElectrum from '../helpers/electrum';
 import { reinstallApp } from '../helpers/setup';
 import {
-  assertAddressMatchesType,
   completeOnboarding,
   dragOnElement,
   elementById,
@@ -23,9 +22,6 @@ import {
   handleOver50PercentAlert,
   handleOver100Alert,
   acknowledgeReceivedPayment,
-  switchAndFundEachAddressType,
-  transferSavingsToSpending,
-  transferSpendingToSavingsAndCloseChannel,
 } from '../helpers/actions';
 import { ciIt } from '../helpers/suite';
 import {
@@ -328,104 +324,4 @@ describe('@onchain - Onchain', () => {
     // await elementByText('OUTPUT (2)').waitForDisplayed({ reverse: true });
   });
 
-  ciIt(
-    '@onchain_multi_address_1 - Receive to each address type and send max combined',
-    async () => {
-      const addressTypes: ('p2pkh' | 'p2sh-p2wpkh' | 'p2wpkh' | 'p2tr')[] = [
-        'p2pkh',
-        'p2sh-p2wpkh',
-        'p2wpkh',
-        'p2tr',
-      ];
-      const satsPerAddressType = 100_000;
-      const { totalFundedSats } = await switchAndFundEachAddressType({
-        addressTypes,
-        satsPerAddressType,
-        waitForSync: async () => {
-          await electrum?.waitForSync();
-        },
-      });
-      const expectedTotal = totalFundedSats
-        .toString()
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-
-      const totalBalance = await elementByIdWithin('TotalBalance-primary', 'MoneyText');
-      await expect(totalBalance).toHaveText(expectedTotal);
-
-      const coreAddress = await getExternalAddress();
-      await enterAddress(coreAddress);
-      await tap('AvailableAmount');
-      await tap('ContinueAmount');
-      await dragOnElement('GRAB', 'right', 0.95);
-      await handleOver50PercentAlert();
-      await elementById('SendSuccess').waitForDisplayed();
-      await tap('Close');
-      await mineBlocks(1);
-      await electrum?.waitForSync();
-
-      const totalBalanceAfter = await elementByIdWithin('TotalBalance-primary', 'MoneyText');
-      await expect(totalBalanceAfter).toHaveText('0');
-    }
-  );
-
-  ciIt(
-    '@onchain_multi_address_2 - Receive to each address type, transfer all to spending, close channel to taproot',
-    async () => {
-      const addressTypes: ('p2pkh' | 'p2sh-p2wpkh' | 'p2wpkh' | 'p2tr')[] = [
-        'p2pkh',
-        'p2sh-p2wpkh',
-        'p2wpkh',
-        'p2tr',
-      ];
-      const satsPerAddressType = 25_000;
-      await switchAndFundEachAddressType({
-        addressTypes,
-        satsPerAddressType,
-        waitForSync: async () => {
-          await electrum?.waitForSync();
-        },
-      });
-
-      // Last funded type is Taproot, keep it as primary for channel open/close.
-      const taprootAddressBeforeClose = await getReceiveAddress();
-      assertAddressMatchesType(taprootAddressBeforeClose, 'p2tr');
-      await swipeFullScreen('down');
-
-      await transferSavingsToSpending({
-        waitForSync: async () => {
-          await electrum?.waitForSync();
-        },
-      });
-
-      // Wait for spending balance to become available before cooperative close.
-      let spendingReady = false;
-      for (let i = 0; i < 12; i++) {
-        const spendingBalanceText = await (
-          await elementByIdWithin('ActivitySpending', 'MoneyText')
-        ).getText();
-        const spendingSats = Number(spendingBalanceText.replace(/[^\d]/g, ''));
-        if (spendingSats > 0) {
-          spendingReady = true;
-          break;
-        }
-        await mineBlocks(1);
-        await electrum?.waitForSync();
-        await sleep(1200);
-      }
-      expect(spendingReady).toBe(true);
-
-      await transferSpendingToSavingsAndCloseChannel({
-        waitForSync: async () => {
-          await electrum?.waitForSync();
-        },
-      });
-
-      const totalBalanceAfterClose = await elementByIdWithin('TotalBalance-primary', 'MoneyText');
-      await expect(totalBalanceAfterClose).not.toHaveText('0');
-
-      const taprootAddressAfterClose = await getReceiveAddress();
-      assertAddressMatchesType(taprootAddressAfterClose, 'p2tr');
-      await swipeFullScreen('down');
-    }
-  );
 });
