7c3d848157e52e95363e472ff9788948b3da4608
Piotr Stachyra <piotr.stachyra@gmail.com>
2026-07-24 12:44:35 +0200

    test: delete paykit profile after sending to contact

--- test/specs/paykit.e2e.ts
+++ test/specs/paykit.e2e.ts
@@ -17,6 +17,7 @@ import {
   addContact,
   cleanupProfile,
   createProfile,
+  deleteProfile,
   discardAddContactRoute,
   verifyAddContactRoute,
   verifyContactRowDisplayed,
@@ -105,6 +106,9 @@ describe('@pubky @paykit - Public payments', () => {
       await expectText('Sent to', { strategy: 'contains' });
       await tap('ContactActivity-1');
       await expectText('10 000');
+
+      await deleteProfile();
+      hasProfile = false;
     } finally {
       if (hasProfile) {
         await cleanupProfile('@paykit_1');
